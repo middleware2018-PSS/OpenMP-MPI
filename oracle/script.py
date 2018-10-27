69f3bb611334af3c9bd20314060bed5401485d2c
Philippe scorsolini <pscorso93@gmail.com>
2018-10-27 15:40:11 +0200

    bug fix

--- oracle/script.py
+++ oracle/script.py
@@ -3,6 +3,7 @@ import json
 from math import sqrt
 from functools import reduce
 import sys
+
 def convert_ts_to_ms(time):
     t = time.strip().split(":")
     if len(t)>1:
@@ -18,7 +19,7 @@ def compute_possession(K, T, m, f, interruptions):
     K=K*10**3
     T=T*10**12
     half=0
-    i=0
+#    i=0
     metadata=json.load(m)
     # convert keys to ints because json do not accept numeric keys directly
     metadata["players"] = {int(k):v for (k,v) in metadata["players"].items()}
@@ -27,7 +28,11 @@ def compute_possession(K, T, m, f, interruptions):
     results={}   # store the results
     lastBall=metadata["game"]["start"]   # used store last ball timestamp, to compute possession attribution
     nextUpdate=metadata["game"]["start"]+T    # compute when next update will be outputted
-#    print("nextUpdate is at {}".format(nextUpdate))
+    players_team = {}
+    for v in metadata["players"].values():
+        players_team[v[0]] = v[2]
+    players_nums= set([el[0] for el in metadata["players"].values()])
+#   print("nextUpdate is at {}".format(nextUpdate))
     for event in interruptions:
         eventType, eventTs = event.strip().split(",")[1:3]
         ms = convert_ts_to_ms(eventTs)
@@ -62,19 +67,20 @@ def compute_possession(K, T, m, f, interruptions):
                 elif sid in metadata["halfs"][half]["balls"]:
 #                   print("ball ", sid, ts)
                     mindist = K
-                    nearest_player = 0
+                    nearest_player = -1
 #                   print(x, y, lastPos)
                     for player_n, vals in lastPos.items():
                         dist = sqrt((vals["center"][0]-x)**2 + (vals["center"][1]-x)**2) 
                         if dist <= mindist:
                             mindist = dist
                             nearest_player = player_n
-		    if nearest_player in metadata["players"].keys():
-			delta = ts - lastBall
-			results[nearest_player]= results[nearest_player] + delta if nearest_player in results.keys() else delta
-			team = metadata["players"][nearest_player][2]
+                    if nearest_player in players_nums:
+                        delta = ts - lastBall
+                        results[nearest_player]= results[nearest_player] + delta if nearest_player in results.keys() else delta
+                        team = players_team[nearest_player]
 #                       print("nearest_player at ts {} to ball {} is {} of team {} with dist of ".format(ts,sid,nearest_player, team, mindist))
-			results[team] = results[team] + delta if team in results.keys() else delta
+                        results[team] = (results[team] + delta) if team in results.keys() else delta
+                    lastBall = ts
 # 	             else:
 #       	           print("no nearest_player {} with K = {} at ts {} to ball {}".format(nearest_player, K*10**-12, ts, sid))
 #             elif i is 0: 
@@ -88,22 +94,22 @@ def compute_possession(K, T, m, f, interruptions):
 #                 e1=x >= metadata["coord"]["x"]["min"]
 #                 f1=x <=metadata["coord"]["x"]["max"]
 #                 print(ts,x,y,eventType, a1 and b1 and ((c1 and c2) or (c3 and c4)) and d1 and e1 and f1)
-	    if ts >= nextUpdate:
-		print(ts,nextUpdate, results)
-		nextUpdate+=T
+        if ts >= nextUpdate:
+            print(ts,nextUpdate, results)
+            nextUpdate+=T
 #    		print("nextUpdate is at {}".format(nextUpdate))
-            if (eventType == "Begin" or eventType == "End") and ts > eventTs:
-#               print("cond: ", eventType, ts)
-                break
+        if (eventType == "Begin" or eventType == "End") and ts > eventTs:
+#           print("cond: ", eventType, ts)
+            break
 
-if len(sys.argv) <6:
-    print("help:\t./script.py K T metadata.json full-game.csv full-game.csv interruptions_stream.csv")
-    print("\tK must be in meters and T in seconds")
-else:
-    K = int(sys.argv[1])
-    T = int(sys.argv[2])
-    metadata_file = sys.argv[3]
-    full_game_file = sys.argv[4]
-    interruptions_file = sys.argv[5]
-    with open(metadata_file) as m, open(full_game_file) as f, open(interruptions_file) as interruptions:
-        compute_possession(K,T,m,f,interruptions)
+#if len(sys.argv) <6:
+#    print("help:\t./script.py K T metadata.json full-game.csv full-game.csv interruptions_stream.csv")
+#    print("\tK must be in meters and T in seconds")
+#else:
+#    K = int(sys.argv[1])
+#    T = int(sys.argv[2])
+#    metadata_file = sys.argv[3]
+#    full_game_file = sys.argv[4]
+#    interruptions_file = sys.argv[5]
+with open("metadata.json") as m, open("full-game.csv") as f, open("interruptions_stream.csv") as interruptions:
+    compute_possession(5,30,m,f,interruptions)
