38a066d20bf14284ed9647b384feab4d7f995143
Philippe scorsolini <pscorso93@gmail.com>
2018-12-01 15:58:45 +0100

    using tmp files

--- merge_referee_sensors.sh
+++ merge_referee_sensors.sh
@@ -1,6 +1,10 @@
-#!/usr/bin/env zsh
+#!/usr/bin/env bash
 
-sort -t , -k2,2 -n \
-  <(cat src/data/referee-events/game-interruption/1st-half.csv | python3 -u oracle/referee_ts_converter.py 10753295594424116 | awk -F, '{print "R,"$3","$2}') \
-  <(cat src/data/referee-events/game-interruption/2nd-half.csv | python3 -u oracle/referee_ts_converter.py 13086639146403495 | awk -F, '{print "R,"$3","$2}') \
-  <(cat data/full-game.csv | awk -F, '{print "S,"$2","$1","$3","$4","$5}')
+first_half=src/data/referee-events/game-interruption/1st-half.csv
+second_half=src/data/referee-events/game-interruption/2nd-half.csv
+full_game=data/full-game.csv
+
+python3 -u oracle/referee_ts_converter.py 10753295594424116 <  $first_half | awk -F, '{print "R,"$3","$2}' | tee game.tmp
+python3 -u oracle/referee_ts_converter.py 13086639146403495 < $second_half | awk -F, '{print "R,"$3","$2}' | tee -a game.tmp
+awk -F, '{print "S,"$2","$1","$3","$4","$5}' $full_game | tee -a game.tmp
+sort -t , -k2,2 -n < game.tmp
