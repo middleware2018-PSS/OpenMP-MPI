976621905d7f8408fb1e79d5b4b35344355d9ff6
Philippe scorsolini <pscorso93@gmail.com>
2018-12-02 14:36:14 +0100

    restore maxs for tests

--- test.sh
+++ test.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 mkdir -p results
-for T in `seq 1 1`;
+for T in `seq 1 60`;
 do
-  for K in `seq 1 1`;
+  for K in `seq 1 5`;
   do
     echo ${K}m ${T}s : >> results/times.txt
     sudo time docker run -v $PWD/data:/data openmp $K $T . > results/result_${K}m_${T}s.txt 2>> results/times.txt;
