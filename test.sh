8f0463a7a5cedd8418e248cc1b36a3344f836082
Philippe scorsolini <pscorso93@gmail.com>
2018-12-06 20:21:09 +0100

    fixed parameters

--- test.sh
+++ test.sh
@@ -5,6 +5,6 @@ do
   for K in `seq 1 5`;
   do
     echo ${K}m ${T}s : >> results/times.txt
-    sudo time docker run -v $PWD/data:/data openmp $K $T . > results/result_${K}m_${T}s.txt 2>> results/times.txt;
+    sudo time docker run -v $PWD/data:/data openmp $K $T . 50 > results/result_${K}m_${T}s.txt 2>> results/times.txt;
   done;
 done
